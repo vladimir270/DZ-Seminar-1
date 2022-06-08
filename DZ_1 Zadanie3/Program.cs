f09995585217e05bc7f7a3d4f746ca9d778f0632
Владимир Ворона <vvorona665@gmail.com>
2022-06-08 22:22:42 +0300

    Задание 3

---
+++ DZ_1 Zadanie3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа которая проверяет является ли число четным или нет
+Console.WriteLine("Введите число");
+int namber = Convert.ToInt32(Console.ReadLine());
+if (namber %2 == 0)
+{
+    Console.WriteLine("Число является четным");
+}  
+else
+{
+    Console.WriteLine("Число не является четным");
+}
+
+
+    
+
