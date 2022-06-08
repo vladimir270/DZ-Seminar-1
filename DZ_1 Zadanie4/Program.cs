4e19038b264f7524f0dfe869a6f8d41adccadc13
Владимир Ворона <vvorona665@gmail.com>
2022-06-08 22:47:25 +0300

    Готво 4 задание

---
+++ DZ_1 Zadanie4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N > 1) 
+{
+    for(int i = 2; i <=N; i+=2)
+    {
+        Console.Write(i);
+        Console.Write(",");
+    }
+}
+if (N < -1);
+{
+    for (int i = -2; i >= N; i -=2)
+    {
+        Console.Write(i);
+        Console.Write(",");
+    }
+}
+if (N <= 1 && N >= -1) Console.WriteLine("Четных чисел нет");
