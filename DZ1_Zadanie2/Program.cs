b9ec1e54234667567dd409a9c4c9253627bb5486
Владимир Ворона <vvorona665@gmail.com>
2022-06-08 21:26:22 +0300

    Готово второе задание

--- DZ1_Zadanie2/Program.cs
+++ DZ1_Zadanie2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа которая находит большее из трех чисел 
+Console.WriteLine("Введите число,  ");
+int namber_A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число,  ");
+int namber_B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число,  ");
+int namber_С = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (namber_A > namber_B)
+{
+    max = namber_A;
+} else {
+    max = namber_B;
+}
+
+if (max < namber_С)
+{
+    Console.WriteLine("Большее число найдено,  ");
+    Console.WriteLine(namber_С);
+} else {
+    Console.WriteLine("Большее число найдено,  ");
+    Console.WriteLine(max);           
+}
