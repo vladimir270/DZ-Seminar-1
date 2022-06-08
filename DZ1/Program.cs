7c17455141014749a6818ea7653e4779101e46b1
Владимир Ворона <vvorona665@gmail.com>
2022-06-08 18:41:33 +0300

    Сделано первое задание

--- DZ1/Program.cs
+++ DZ1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа которая находит большее из двух чисел 
+Console.WriteLine("Введите число,  ");
+int namber_A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число,  ");
+int namber_B = Convert.ToInt32(Console.ReadLine());
+if (namber_A > namber_B)
+{ 
+    Console.WriteLine("Первое число больше");
+}
+
+else
+{   
+    Console.WriteLine("Второе число больше");
+}
